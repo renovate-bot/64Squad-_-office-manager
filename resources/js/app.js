229ab98fce9ef9eeaee15c4cff13c79441359922
Restyled.io <commits@restyled.io>
2023-08-24 20:09:43 +0000

    Restyled by prettier

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
+import "./bootstrap";
 
-import './bootstrap';
-
-import focus from '@alpinejs/focus';
-import Alpine from 'alpinejs';
+import focus from "@alpinejs/focus";
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-
-import laravel, {refreshPaths} from 'laravel-vite-plugin';
-import {defineConfig} from 'vite';
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins : [
-    laravel({
-      input : [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
-      refresh : [
-        ...refreshPaths,
-        'app/Http/Livewire/**',
-      ],
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
+        }),
+    ],
 });
